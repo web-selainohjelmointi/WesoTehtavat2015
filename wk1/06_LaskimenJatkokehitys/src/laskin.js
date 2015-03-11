89c61fd3929b1c9e754b86fca3a7e43ff660b89f
avihavai <avihavai@XX.helsinki.fi>
2015-03-12 01:05:34 +0200

    wk1 exs

---
+++ wk1/06_LaskimenJatkokehitys/src/laskin.js
@@ -0,0 +1,15 @@
+function haeNumero(tunnus) {
+    return parseInt(document.getElementById(tunnus).value);
+}
+
+function asetaTulos(tulos) {
+    document.getElementById("tulos").innerHTML = tulos;
+}
+
+function plus() {
+    asetaTulos(haeNumero("eka") + haeNumero("toka"));
+}
+
+function kerto() {
+    asetaTulos(haeNumero("eka") * haeNumero("toka"));
+}
---
+++ wk1/08_DOMWalker/src/code.js
@@ -0,0 +1,3 @@
+function init() {
+    // implement your solution for the dom walking and printing here
+}
