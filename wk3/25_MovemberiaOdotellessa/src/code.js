3605cb871c916c027ead9531f5aa9dd8d8df95a6
avihavai <avihavai@XX.helsinki.fi>
2015-03-27 01:02:32 +0200

    wk3

---
+++ wk3/25_MovemberiaOdotellessa/src/code.js
@@ -0,0 +1,27 @@
+// valmis data
+var data = [{
+        title: "PerusMOOC?",
+        paragraphs: [
+            {paragraph: "Helsingin yliopiston tietojenkäsittelytieteen laitos järjestää kokonaan etäopiskeluna suoritettavan kaikille avoimen verkkokurssin tietokoneohjelmoinnista (MOOC). Kurssi on tehtäväperustainen siten, että oppimateriaali \"opettaa itse itsensä\" tehtävistä saatavan automaattisen palautteen kautta. Kurssi on suunnattu peruskoulun yläkoululaisille, ja kurssilla pääsee tekemään muunmuassa matopelin."},
+            {paragraph: "Koulusi voi osallistua PerusMOOCiin ilmoittautumalla sähköpostitse osoitteeseen mooc@cs.helsinki.fi. Voit myös ilmoittaa kurssista opiskelijoillesi, jotka saavat käydä kurssia vapaasti."},
+            {paragraph: "Kurssi on kaikille ilmainen!"}
+        ]
+    },
+    {
+        title: "Materiaali",
+        paragraphs: [
+            {paragraph: "Polku kohti tekstiseikkailua..."},
+            {paragraph: "Polku kohti animaatioita..."},
+            {paragraph: "Polku kohti käyttäjän näppäimistön käsittelyä..."}
+        ]
+    },
+    {
+        title: "Oma etenemiseni",
+        paragraphs: [
+            {paragraph: "TBA!"}
+        ]
+    }];
+
+function init() {
+
+}
